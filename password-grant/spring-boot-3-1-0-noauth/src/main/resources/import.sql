8cde4665150b2524e1a783e64e0cd52f44d452f5
Lucas <lalmeida464@gmail.com>
2024-11-18 22:29:09 -0300

    User Role model / Spring Security

---
+++ password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -0,0 +1,8 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+
+INSERT INTO tb_product (name) VALUES ('TV');
+INSERT INTO tb_product (name) VALUES ('Computer');
+
+INSERT INTO tb_product (name) VALUES ('TV');
+INSERT INTO tb_product (name) VALUES ('Computer');
